e7f34815e98d01b0e41f74ce5d24fd6b5c4789cd
Yegor Bugayenko <yegor256@gmail.com>
2023-05-26 11:03:05 +0800

    #12 year up

--- test/test_veil.rb
+++ test/test_veil.rb
@@ -2,7 +2,7 @@
 
 # (The MIT License)
 #
-# Copyright (c) 2020 Yegor Bugayenko
+# Copyright (c) 2020-2023 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -28,7 +28,7 @@ require_relative '../lib/veil'
 
 # Veil test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2020 Yegor Bugayenko
+# Copyright:: Copyright (c) 2020-2023 Yegor Bugayenko
 # License:: MIT
 class VeilTest < Minitest::Test
   def test_simple
